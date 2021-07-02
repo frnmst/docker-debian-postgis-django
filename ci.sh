4d60a2420b1ab98265948fc35d22dd87a4431e42
Franco Masotti <franco.masotti@live.com>
2021-07-02 16:40:44 +0200

    Fixes.

--- ci.sh
+++ ci.sh
@@ -24,6 +24,8 @@ set -euo pipefail
 
 export MAKEFILE_URL="${1}"
 export MAKEFILE_CHECKSUM="${2}"
+export UWSGI_INI_URL="${3}"
+export UWSGI_INI_CHECKSUM="${4}"
 
 [ -n "${ENV}" ]
 
@@ -64,8 +66,11 @@ ${CURL} https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 pip3 install pipenv
 
+# Get the required files.
 ${CURL} "${MAKEFILE_URL}" --output Makefile \
     && [ "$(${SHA512SUM} Makefile | ${AWK} '{print $1}')" = "${MAKEFILE_CHECKSUM}" ]
+${CURL} "${UWSGI_INI_URL}" --output uwsgi.ini \
+    && [ "$(${SHA512SUM} uwsgi.ini | ${AWK} '{print $1}')" = "${UWSGI_INI_CHECKSUM}" ]
 
 pipenv lock --requirements > requirements.txt
 if [ "${ENV}" = 'development' ]; then
